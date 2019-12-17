3d3b6ecb224a744969d569fbb8aad588bc4a63e5
Kenneth Frandsen <Kf@mustache.dk>
2019-12-17 15:03:48 +0100

    Initial Commit

---
+++ Reachablility/Reachablility.h
@@ -0,0 +1,19 @@
+//
+//  Reachablility.h
+//  Reachablility
+//
+//  Created by Kenneth Frandsen on 17/12/2019.
+//  Copyright © 2019 Mustache. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Reachablility.
+FOUNDATION_EXPORT double ReachablilityVersionNumber;
+
+//! Project version string for Reachablility.
+FOUNDATION_EXPORT const unsigned char ReachablilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reachablility/PublicHeader.h>
+
+
